4266366efe27baf99ecd335af8303d0675fde981
Tom Sain <tom.sain@Nology.local>
2019-05-14 20:21:37 +0100

    create react app with typescript

---
+++ front_end/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Front-end code will go here</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
