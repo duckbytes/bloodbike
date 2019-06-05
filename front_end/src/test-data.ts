64fdfa9290e9d4180ff3a1b26999c38c0a1d1bf3
renewabristol <thomas.a.sain@gmail.com>
2019-06-05 05:21:46 +0100

    styling

--- front_end/src/test-data.ts
+++ front_end/src/test-data.ts
@@ -2,27 +2,75 @@ export interface IJob {
   timeOfCall: string;
   contents: string;
   from: string;
-  // to: string;
-  // rider: "North" | "West" | "East" | "Relief";
-  // collected: number | "not colleced";
-  // delivered: number | "en route"
+  to: string;
+  rider: string;
+  collected: number | string;
+  delivered: number | string;
 }
 
 export const testData: IJob[] = [
   {
     timeOfCall: "TOC",
     contents: "Contents",
-    from: "From"
+    from: "From",
+    to: "To",
+    rider: "Rider",
+    collected: "Collected at",
+    delivered: "Delivered at"
   },
   {
     timeOfCall: "1907",
     contents: "blood",
-    from: "Eliz.Casson unit"
+    from: "Eliz.Casson unit",
+    to: "BRI A&E",
+    rider: "North",
+    collected: "1938",
+    delivered: "2005"
   },
   {
     timeOfCall: "1907",
     contents: "milk",
-    from: "SoutmeadNICU"
+    from: "SoutmeadNICU",
+    to: "Weston Handover",
+    rider: "West",
+    collected: "1940",
+    delivered: "2030"
+  },
+  {
+    timeOfCall: "1907",
+    contents: "blood",
+    from: "Eliz.Casson unit",
+    to: "BRI A&E",
+    rider: "North",
+    collected: "1938",
+    delivered: "2005"
+  },
+  {
+    timeOfCall: "1907",
+    contents: "milk",
+    from: "SoutmeadNICU",
+    to: "Weston Handover",
+    rider: "West",
+    collected: "1940",
+    delivered: "2030"
+  },
+  {
+    timeOfCall: "1907",
+    contents: "blood",
+    from: "Eliz.Casson unit",
+    to: "BRI A&E",
+    rider: "North",
+    collected: "1938",
+    delivered: "2005"
+  },
+  {
+    timeOfCall: "1907",
+    contents: "milk",
+    from: "SoutmeadNICU",
+    to: "Weston Handover",
+    rider: "West",
+    collected: "1940",
+    delivered: "2030"
   }
 ];
 export default testData;
